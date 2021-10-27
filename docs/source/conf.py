ff1d0b0e210cad601d6809384927638186124543
bhausleitner <bernhard.hausleitner@tum.de>
2021-10-27 10:32:04 -0700

    create build for deploy and updated favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,16 +14,17 @@
 
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
+
+# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 from typing import Dict, Any
 
 # -- Project information -----------------------------------------------------
 
-project = 'NeuralProphet'
-copyright = '2021, Oskar Triebe'
-author = 'Oskar Triebe'
+project = "NeuralProphet"
+copyright = "2021, Oskar Triebe"
+author = "Oskar Triebe"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,23 +33,23 @@ author = 'Oskar Triebe'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc', 
-    'sphinx.ext.coverage', 
-    'sphinx.ext.napoleon',
-    'nbsphinx',
-    'nbsphinx_link',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "nbsphinx",
+    "nbsphinx_link",
 ]
 
-#Here to describe what format of files are parsed
+# Here to describe what format of files are parsed
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -56,21 +57,20 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
-html_favicon = "images/swiss_favicon.png"
-html_logo ="images/np_logo.png"
+html_theme = "furo"
+html_favicon = "images/np_favicon.png"
+html_logo = "images/np_logo.png"
 html_theme_options: Dict[str, Any] = {
-    'sidebar_hide_name': True,
-    'navigation_with_keys': True,
+    "sidebar_hide_name": True,
+    "navigation_with_keys": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
